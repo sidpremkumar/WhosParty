4f838ae055dced57ebc294e28f256d78ecdd9527
Sidhartha Premkumar <spremkum@redhat.com>
2020-04-23 13:52:11 -0400

    Change to new model with race classification support

--- WhosParty/detect_gender.py
+++ WhosParty/detect_gender.py
@@ -1,83 +1,104 @@
-# author: Arun Ponnusamy
-# website: https://www.arunponnusamy.com
+# Original Author:
 
-# import necessary packages
-from keras.preprocessing.image import img_to_array
-from keras.models import load_model
-from keras.utils import get_file
-import numpy as np
+from __future__ import print_function
 import argparse
-import cv2
 import os
-import cvlib as cv
+import face_recognition
+import numpy as np
+import sklearn
+import pickle
+from face_recognition import face_locations
+from PIL import Image, ImageDraw, ImageFont
+from tqdm import tqdm
+import cv2
+import pandas as pd
+# we are only going to use 4 attributes
+COLS = ['Male', 'Asian', 'White', 'Black']
+N_UPSCLAE = 1
+def extract_features(img_path):
+    """Exctract 128 dimensional features
+    """
+    X_img = face_recognition.load_image_file(img_path)
+    locs = face_locations(X_img, number_of_times_to_upsample = N_UPSCLAE)
+    if len(locs) == 0:
+        return None, None
+    face_encodings = face_recognition.face_encodings(X_img, known_face_locations=locs)
+    return face_encodings, locs
 
-class DetectGender:
-	model_path = 0;
+def predict_one_image(img_path, clf, labels):
+    """Predict face attributes for all detected faces in one image
+    """
+    face_encodings, locs = extract_features(img_path)
+    if not face_encodings:
+        return None, None
+    pred = pd.DataFrame(clf.predict_proba(face_encodings),
+                        columns = labels)
+    pred = pred.loc[:, COLS]
+    return pred, locs
+def draw_attributes(img_path, df):
+    """Write bounding boxes and predicted face attributes on the image
+    """
+    img = cv2.imread(img_path)
+    # img  = cv2.cvtColor(color, cv2.COLOR_BGR2RGB)
+    for row in df.iterrows():
+        top, right, bottom, left = row[1][4:].astype(int)
+        if row[1]['Male'] >= 0.5:
+            gender = 'Male'
+        else:
+            gender = 'Female'
 
-	def __init__(self):
-		"""
-		Constructor to download pre-trained model
-		"""
-		# download pre-trained model file (one-time download)
-		dwnld_link = "https://github.com/arunponnusamy/cvlib/releases/download/v0.2.0/gender_detection.model"
-		self.model_path = get_file("gender_detection.model", dwnld_link,
-		                     cache_subdir="pre-trained", cache_dir=os.getcwd())
+        race = np.argmax(row[1][1:4])
+        text_showed = "{} {}".format(race, gender)
 
-	def classifyImage(self, file_path):
-		"""
-		Function to classify an image and return classification
-		:param String file_path: Path to the image
-		Returns list [man confidence, women confidence]
-		"""
-		# read input image
-		image = cv2.imread(file_path)
-
-		if image is None:
-		    raise Exception("Could not read input image")
-
-		# load pre-trained model
-		model = load_model(self.model_path)
-
-		# detect faces in the image
-		face, confidence = cv.detect_face(image)
-
-		classes = ['man','woman']
-
-		# loop through detected faces
-		try:
-			for idx, f in enumerate(face):
-			    # get corner points of face rectangle
-			    (startX, startY) = f[0], f[1]
-			    (endX, endY) = f[2], f[3]
-
-			    # draw rectangle over face
-			    cv2.rectangle(image, (startX,startY), (endX,endY), (0,255,0), 2)
-
-			    # crop the detected face region
-			    face_crop = np.copy(image[startY:endY,startX:endX])
-
-			    # preprocessing for gender detection model
-			    face_crop = cv2.resize(face_crop, (96,96))
-			    face_crop = face_crop.astype("float") / 255.0
-			    face_crop = img_to_array(face_crop)
-			    face_crop = np.expand_dims(face_crop, axis=0)
-
-			    # apply gender detection on face
-			    conf = model.predict(face_crop)[0]
-			    return conf, classes
-		except Exception as e:
-			raise e
+        cv2.rectangle(img, (left, top), (right, bottom), (0, 0, 255), 2)
+        font = cv2.FONT_HERSHEY_DUPLEX
+        img_width = img.shape[1]
+        cv2.putText(img, text_showed, (left + 6, bottom - 6), font, 0.5, (255, 255, 255), 1)
+    return img
 
 
-		#
-		# # display output
-		# cv2.imshow("gender detection", image)
-		#
-		# # press any key to close window
-		# cv2.waitKey()
-		#
-		# # save output
-		# cv2.imwrite("gender_detection.jpg", image)
-		#
-		# # release resources
-		# cv2.destroyAllWindows()
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--img_dir', type=str,
+                        default='test/', required = True,
+                        help='input image directory (default: test/)')
+    parser.add_argument('--output_dir', type=str,
+                        default='results/',
+                        help='output directory to save the results (default: results/')
+    parser.add_argument('--model', type=str,
+                        default='face_model.pkl', required = True,
+                        help='path to trained model (default: face_model.pkl)')
+
+    args = parser.parse_args()
+    output_dir = args.output_dir
+    input_dir = args.img_dir
+    model_path = args.model
+
+    if not os.path.isdir(output_dir):
+        os.mkdir(output_dir)
+    # load the model
+    with open(model_path, 'rb') as f:
+        clf, labels = pickle.load(f, encoding='latin1')
+
+    print("classifying images in {}".format(input_dir))
+    for fname in tqdm(os.listdir(input_dir)):
+        img_path = os.path.join(input_dir, fname)
+        try:
+            pred, locs = predict_one_image(img_path, clf, labels)
+        except:
+            print("Skipping {}".format(img_path))
+        if not locs:
+            continue
+        locs = \
+            pd.DataFrame(locs, columns = ['top', 'right', 'bottom', 'left'])
+        df = pd.concat([pred, locs], axis=1)
+        img = draw_attributes(img_path, df)
+        cv2.imwrite(os.path.join(output_dir, fname), img)
+        os.path.splitext(fname)[0]
+        output_csvpath = os.path.join(output_dir,
+                                      os.path.splitext(fname)[0] + '.csv')
+        df.to_csv(output_csvpath, index = False)
+
+if __name__ == "__main__":
+    main()
